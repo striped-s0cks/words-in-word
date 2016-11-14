4c15815d8f3a7ae1e756ae64474eb3432b51e725
Yana Savchenko <yana.savchenko@amsurg.com>
2016-11-14 15:18:09 -0600

    Add util to get new word and generate combinations

---
+++ src/utils/wordUtil.js
@@ -0,0 +1,43 @@
+import cmb from 'js-combinatorics';
+import _   from 'lodash';
+
+import words from './words';
+
+const MIN_LENGTH = 7;
+
+function getRandomWord() {
+    let word;
+
+    while ( !word ) {
+        const generatedWord = words[ Math.floor( Math.random() * words.length ) ];
+
+        if ( generatedWord.length >= MIN_LENGTH ) {
+            word = generatedWord;
+        }
+    }
+
+    return word;
+}
+
+function getAllWords(word) {
+    const subsets = cmb.permutationCombination(word.split(''));
+    const allWords = [];
+
+    subsets.forEach( item => {
+        const joined = item.join('');
+
+        if ( ( words.indexOf(joined) > -1 ) && ( joined !== word ) ) {
+            allWords.push({
+                value: joined,
+                isShown: true
+            });
+        }
+    });
+
+    return _.uniqBy(allWords, 'value');
+}
+
+module.exports = {
+    getRandomWord,
+    getAllWords
+};
