2123a3e699bdef8068d25ebadd4c93acdcb018b7
Yana Savchenko <yana.savchenko@amsurg.com>
2016-11-15 12:46:23 -0600

    Remove redundant loaders

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,10 @@ global.Promise = require('bluebird');
 
 module.exports = {
     entry: "./src/main.js",
-    plugins:[
+    plugins: [
         new webpack.DefinePlugin({
             'process.env': {
                 NODE_ENV: JSON.stringify( process.env.NODE_ENV || 'development' )
-            }
-        }),
-
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: true
             }
         })
     ],
@@ -28,11 +22,6 @@ module.exports = {
         loaders: [
             { test: /\.css$/, loader: "style-loader!css-loader" },
             { test: /\.less$/, loader: "style-loader!css-loader!less-loader"},
-            { test: /\.gif$/, loader: "url-loader?limit=10000&mimetype=image/gif" },
-            { test: /\.jpg$/, loader: "url-loader?limit=10000&mimetype=image/jpg" },
-            { test: /\.png$/, loader: "url-loader?limit=10000&mimetype=image/png" },
-            { test: /\.svg/, loader: "url-loader?limit=26000&mimetype=image/svg+xml" },
-            { test: /\.(woff|woff2|ttf|eot)/, loader: "url-loader?limit=1" },
             { test: /\.jsx$/, loader: "react-hot!babel!eslint-loader", exclude: [/node_modules/] },
             { test: /\.js$/, loader: "babel!eslint-loader", exclude: [/node_modules/] },
             { test: /\.json$/, loader: "json-loader"}
