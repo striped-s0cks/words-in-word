6afef23d00c50df74b89b2232a1858b2b5d88616
Yana Savchenko <yana.savchenko@amsurg.com>
2016-11-15 16:10:31 -0600

    Add warning when no words

--- src/components/other/AllWords.jsx
+++ src/components/other/AllWords.jsx
@@ -8,18 +8,34 @@ export default class AllWords extends React.Component {
     render() {
         return (
             <div className='AllWords'>
-                <ul>
-                    {
-                        this.props.words.map( (item, key) => {
-                            return (
-                                <li key={key}>
-                                    <Word value={item} />
-                                </li>
-                            );
-                        })
-                    }
-                </ul>
+                {this._renderWords()}
             </div>
+        );
+    }
+
+    _renderWords() {
+        if ( this.props.words.length ) {
+            return (
+                <div className='list'>
+                    <ul>
+                        {
+                            this.props.words.map( (item, key) => {
+                                return (
+                                    <li key={key}>
+                                        <Word value={item} />
+                                    </li>
+                                );
+                            })
+                        }
+                    </ul>
+                </div>
+            );
+        }
+
+        return (
+            <p className='warning'>
+                No words. Maybe word is too short or does not exist
+            </p>
         );
     }
 }
