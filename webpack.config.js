1a927fa06b124c37f8e4f31618f917aa6b0a37b4
Yana Savchenko <yana.savchenko@amsurg.com>
2016-11-15 12:40:52 -0600

    Remove warning

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,7 @@ module.exports = {
     entry: "./src/main.js",
     plugins: [
         new webpack.DefinePlugin({
-            "process.env": {
-                NODE_ENV: JSON.stringify( process.env.NODE_ENV || 'development' )
-            }
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
         })
     ],
 
