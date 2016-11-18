c5c44919ab922deea775861132d0e5f5b578d0ce
YanaSavchenko <yanka.savchenko@gmail.com>
2016-11-18 12:50:52 -0600

    Update nightwatch.config.js

--- nightwatch.config.js
+++ nightwatch.config.js
@@ -1,11 +1,9 @@
 module.exports = (
     function(settings) {
         settings.test_settings.default.launch_url = __dirname + '/index.html';
-        console.log(settings)
 
         return settings;
     }
 )(
-    require('./nightwatch.json'),
-    require('babel-register')
+    require('./nightwatch.json')
 );
