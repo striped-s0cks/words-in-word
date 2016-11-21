52557ac91829369d24e1e8c7eab08ee79eb0f5db
Yana Savchenko <yana.savchenko@amsurg.com>
2016-11-21 12:47:41 -0600

    Add test to check generation of word

---
+++ tests/test/appPage/generateWord.js
@@ -0,0 +1,58 @@
+'use strict';
+
+module.exports = {
+    'Generate word': client => {
+        const appPage = client.page.appPage();
+        const buttons = appPage.section.buttons;
+
+        appPage.open();
+
+        appPage.waitForElementVisible('@appPage', 3000);
+        appPage.waitForElementVisible('@buttons', 3000);
+        appPage.waitForElementVisible('@initialWord', 3000);
+
+        let oldWord = '';
+        let newWord = '';
+
+        client.elements('class name', 'CharBox', function(res) {
+            for ( let index = 1; index <= res.value.length; index++ ) {
+                const className = `.CharBox:nth-child(${index})`;
+
+                this.getText(className, function(text){
+                    oldWord += text.value;
+                });
+            }
+        }).perform( function() {
+            buttons.click('@generateButton');
+        }).perform( function() {
+            appPage.waitForElementVisible('@initialWord', 3000);
+
+            client.elements('class name', 'CharBox', function(res) {
+            for ( let index = 1; index <= res.value.length; index++ ) {
+                const className = `.CharBox:nth-child(${index})`;
+
+                this.getText(className, function(text){
+                    newWord += text.value;
+                });
+            }
+            });
+        }).perform( function() {
+            client.assert.notEqual(oldWord, newWord);
+        });
+
+        appPage.waitForElementVisible('@userWord', 3000);
+        appPage.expect.element('@userWord').text.to.equal('');
+
+        appPage.waitForElementVisible('@allWords', 3000);
+        client.elements('class name', 'Word', function(res) {
+            for ( let index = 1; index <= res.value.length; index++ ) {
+                const className = `li:nth-child(${index}) .Word`;
+
+                this.expect.element(className).text.to.not.match(/[a-z]+/);
+            }
+        });
+
+        client.pause(2000);
+        client.end();
+    }
+};
